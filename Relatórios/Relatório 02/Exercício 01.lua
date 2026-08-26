447f0868cfc44415f5c55726d9577c13e3f57061
Maria Clara Freitas <142271355+myverita@users.noreply.github.com>
2026-08-26 13:15:18 -0300

    Update Exercício 01.lua

--- Relatórios/Relatório 02/Exercício 01.lua
+++ Relatórios/Relatório 02/Exercício 01.lua
@@ -1,1 +1,20 @@
+print("DIGITE O EXPOENTE INICIAL")
 
+local exp1 = tonumber(io.read())
+
+print("DIGITE O EXPOENTE FINAL")
+
+local exp2 = tonumber(io.read())
+
+print("DIGITE A BASE")
+
+local base = tonumber(io.read())
+
+if exp1 and exp2 then 
+    for i = exp1, exp2 do
+        local resul = base ^ i
+        print(base.. " ^ " .. i .. " = " .. resul)
+    end
+else
+    print("ENTRADA INVALIDA")
+end
