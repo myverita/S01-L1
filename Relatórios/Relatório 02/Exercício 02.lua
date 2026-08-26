7209c786579116d44d924db3b4b04fbe4871203a
Maria Clara Freitas <142271355+myverita@users.noreply.github.com>
2026-08-26 13:31:31 -0300

    Update Exercício 02.lua

--- Relatórios/Relatório 02/Exercício 02.lua
+++ Relatórios/Relatório 02/Exercício 02.lua
@@ -1,1 +1,34 @@
+local tabela1 = {}
 
+print("DIGITE O TAMANHO DA TABELA")
+
+local tamanho = tonumber(io.read())
+
+print("DIGITE OS NUMEROS DA TABELA")
+
+if tamanho then 
+    for i = 1, tamanho do
+        local novo = tonumber(io.read())
+        table.insert(tabela1, novo)
+    end
+else
+    print("ENTRADA INVALIDA")
+end
+
+print("DIGITE O NUMERO A SER BUSCADO")
+
+local buscar = tonumber(io.read())
+
+function contarOcorrencias(tabela, alvo)
+    local ocorr = 0
+    for i = 1, #tabela do
+        if tabela[i] == alvo then
+            ocorr = ocorr + 1
+        end
+    end
+    return ocorr
+end
+
+local resul = contarOcorrencias(tabela1, buscar)
+
+print("O NUMERO " .. buscar .." APARECE " .. resul .. " VEZES NA TABELA")
