1c6a590098c10947fd0a0c1916904b2c9ce89a3a
Maria Clara Freitas <142271355+myverita@users.noreply.github.com>
2026-09-10 14:11:46 -0300

    Update Exercício 03.go

--- Relatórios/Relatório 04/Exercício 03.go
+++ Relatórios/Relatório 04/Exercício 03.go
@@ -1,1 +1,18 @@
+package main
+import "fmt"
 
+func gerarEscalaPlantao(n int) {
+	fmt.Println("escala de plantão tecnico")
+	dia := 1
+	for i := 1; i <= n; i++ {
+		fmt.Println("Plantao", i, ": dia", dia, "do mes")
+		dia = dia + 4
+	}
+}
+
+func main() {
+	var quant int
+	fmt.Print("Digite a quantidade de plantões necessários: ")
+	fmt.Scan(&quant)
+	gerarEscalaPlantao(quant)
+}
