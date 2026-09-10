8c62ef7e0f307b4a223768ae4020c3ba6c97b393
Maria Clara Freitas <142271355+myverita@users.noreply.github.com>
2026-09-10 14:17:14 -0300

    Update Exercício 04.go

--- Relatórios/Relatório 04/Exercício 04.go
+++ Relatórios/Relatório 04/Exercício 04.go
@@ -1,1 +1,31 @@
+package main
+
+import "fmt"
+
+func validarIngresso(setor string, codigo int) bool {
+	if setor == "VIP" && codigo == 2026 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	var setor string
+	var cod int
+	var valido bool
+	for {
+		fmt.Print("Digite o setor do ingresso: ")
+		fmt.Scan(&setor)
+		fmt.Print("Digite o codigo do ingresso: ")
+		fmt.Scan(&cod)
+		valido = validarIngresso(setor, cod)
+		if valido == true {
+			fmt.Println("Acesso liberado a area VIP")
+			break
+		} else {
+			fmt.Println("Ingresso ou setor invalido. Tente novamente.")
+		}
+	}
+}
 
