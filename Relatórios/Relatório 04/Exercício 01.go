6577a11107d3e5a1793dc75f617f4b3739afafad
Maria Clara Freitas <142271355+myverita@users.noreply.github.com>
2026-09-10 14:02:32 -0300

    Update Exercício 01.go

--- Relatórios/Relatório 04/Exercício 01.go
+++ Relatórios/Relatório 04/Exercício 01.go
@@ -1,1 +1,22 @@
+package main
+import "fmt"
 
+func ValidarCodigoRastreio(codigo string) (bool, string) {
+	if len(codigo) == 10 {
+		return true, "Código de rastreio registrado no sistema!"
+	} else {
+		return false, "Erro: O código de rastreio deve ter exatamente 10 caracteres."
+	}
+}
+
+func main() {
+	var cod string
+	var val bool
+	var msgm string
+	for val == false {
+		fmt.Print("Digite o código de rastreio: ")
+		fmt.Scan(&cod)
+		val, msgm = ValidarCodigoRastreio(cod)
+		fmt.Println(msgm)
+	}
+}
