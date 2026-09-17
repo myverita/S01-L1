eb21768f329df49d0a1660fbf954333d0779893f
Maria Clara Freitas <142271355+myverita@users.noreply.github.com>
2026-09-17 13:45:16 -0300

    Update Exercício 01.cpp

--- Relatórios/Relatório 05/Exercício 01.cpp
+++ Relatórios/Relatório 05/Exercício 01.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int combinar_equipes(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    return combinar_equipes(n - 1) + combinar_equipes(n - 2);
+}
+
+int main() {
+    int n;
+
+    cout << "Digite o tamanho do chaveamento: ";
+    cin >> n;
+    cout << "Total de cenarios de confrontos possiveis: " << combinar_equipes(n) << endl;
+
+    return 0;
+}
